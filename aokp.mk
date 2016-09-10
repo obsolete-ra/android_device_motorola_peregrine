c9f91991f0e62eea54c93c73f0bb6eb2927d8690
Rob E Anderson <obsolete.ra@gmail.com>
2016-09-10 11:42:35 -0400

    aokp.mk: typo repair

--- aokp.mk
+++ aokp.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
 PRODUCT_NAME := aokp_peregrine
