e5afe240f1e8e0f5071de8bc7c2b1d3a51a8ad33
Rob E Anderson <obsolete.ra@gmail.com>
2016-09-10 11:37:01 -0400

    Setup for AOKP-N

--- aokp.mk
+++ aokp.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := cm_peregrine
+PRODUCT_NAME := aokp_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
